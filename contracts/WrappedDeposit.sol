77a4b70dbd4f62217054893fe9295dfb4d7f1c1e
Chance Hudson <jchancehud@gmail.com>
2021-12-09 14:46:29 -0600

    refactor: different interfaces for different assets

--- contracts/WrappedDeposit.sol
+++ contracts/WrappedDeposit.sol
@@ -1,18 +1,36 @@
 /// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-interface DepositReceiver {
-  function acceptDeposit(address depositor, address token, uint amount) external;
+interface ERC20Receiver {
+  function acceptERC20Deposit(address depositor, address token, uint amount) external returns (bool);
+}
+
+interface ERC721Receiver {
+  function acceptERC721Deposit(address depositor, address token, uint tokenid) external returns (bool);
+}
+
+interface EtherReceiver {
+  function acceptEtherDeposit(address depositor, uint amount) external returns (bool);
 }
 
 interface IERC20 {
   function transferFrom(address sender, address recipient, uint amount) external returns (bool);
 }
 
+interface IERC721 {
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+  function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory data) external payable;
+}
+
 contract WrappedDeposit {
-  function depositToken(address to, address token, uint amount) public {
-    require(_isContract(to));
-    DepositReceiver(to).acceptDeposit(msg.sender, token, amount);
+  function depositERC20(address to, address token, uint amount) public {
+    require(_isContract(to), "noncontract");
+    tryExecute(to, abi.encodeWithSelector(
+      ERC20Receiver(to).acceptERC20Deposit.selector,
+      msg.sender,
+      token,
+      amount
+    ));
     bytes memory data = abi.encodeWithSelector(
       IERC20(token).transferFrom.selector,
       msg.sender,
@@ -27,11 +45,48 @@ contract WrappedDeposit {
     }
   }
 
+  function depositERC721(address to, address token, uint tokenId) public {
+    require(_isContract(to), "noncontract");
+    tryExecute(to, abi.encodeWithSelector(
+      ERC721Receiver(to).acceptERC721Deposit.selector,
+      msg.sender,
+      token,
+      tokenId
+    ));
+    IERC721(token).transferFrom(msg.sender, to, tokenId);
+  }
+
+  function safeDepositERC721(address to, address token, uint tokenId, bytes memory data) public {
+    require(_isContract(to), "noncontract");
+    tryExecute(to, abi.encodeWithSelector(
+      ERC721Receiver(to).acceptERC721Deposit.selector,
+      msg.sender,
+      token,
+      tokenId
+    ));
+    IERC721(token).safeTransferFrom(msg.sender, to, tokenId, data);
+  }
+
   function depositEther(address to) public payable {
-    require(_isContract(to));
-    DepositReceiver(to).acceptDeposit(msg.sender, address(0), msg.value);
+    require(_isContract(to), "noncontract");
+    tryExecute(to, abi.encodeWithSelector(
+      EtherReceiver(to).acceptEtherDeposit.selector,
+      msg.sender,
+      msg.value
+    ));
     (bool success, ) = to.call{value: msg.value}('');
     require(success);
+  }
+
+  /**
+   * Try to call a function that should return a boolean. If anything other
+   * than true is returned revert.
+   **/
+  function tryExecute(address to, bytes memory data) private {
+    (bool success, bytes memory returndata) = to.call(data);
+    require(success);
+    require(returndata.length > 0);
+    require(abi.decode(returndata, (bool)));
   }
 
   function _isContract(address c) private view returns (bool) {
